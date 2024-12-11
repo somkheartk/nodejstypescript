7257bb3d8f95655bd7eaa51df4ac787874936680
somkheartk <43743072+somkheartk@users.noreply.github.com>
2024-12-11 10:52:43 +0700

    add

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ const PORT = parseInt(process.env.PORT || '3000', 10);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
+}); 
 
 
 export default app;
